62daa6bb78926c33f0df07376b2d01dc9ed44ae9
Siya Dadheech <162879979+SiyaDadheech@users.noreply.github.com>
2026-04-25 17:38:07 +0530

    Update api.js

--- Frontend/hospiosk-main/src/services/api.js
+++ Frontend/hospiosk-main/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+//const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = "https://hospioskk.onrender.com";
 
 // ---------- PATIENT ----------
 export const getPatientByAadhaar = async (aadhaar) => {
